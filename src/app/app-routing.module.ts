9316d1f4a0bccea398cbeefaa8ee776555571996
Shpendsermaxhaj <43429428+Shpendsermaxhaj@users.noreply.github.com>
2019-09-07 09:08:01 -0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MobileComponent } from './mobile/mobile.component';
+import { TestComponent } from './test/test.component';
+import { PizzatestComponent } from './pizzatest/pizzatest.component';
+import { CoverComponent } from './cover/cover.component';
 
-const routes: Routes = [];
+// const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'menu', component: MenuComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'mobile', component: MobileComponent },
+      { path: 'test', component: TestComponent },
+      { path: 'pizzatest', component: PizzatestComponent },
+      { path: 'cover', component: CoverComponent },
+      { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    ])
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
